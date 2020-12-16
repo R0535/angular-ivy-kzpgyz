224d737ed2b74b895447dd7d654b9a989b0561f5
R0535 <jghr053@gmail.com>
2020-12-15 23:15:57 -0600

    hacia code

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,16 +1,44 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from "@angular/core";
 import { Product } from "../models/product.model";
 
 @Component({
   selector: "app-product",
-  templateUrl: "./product.component.html"
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"]
 })
-export class ProductComponent {
+export class ProductComponent implements OnChanges, OnInit, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
+  remove() {
+    this.productClicked.emit(this.product.id);
+  }
   addCart() {
     console.log("anadir al carrito");
     this.productClicked.emit(this.product.id);
+  }
+  constructor() {
+    console.log("construccion");
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges");
+    console.log(changes);
+  }
+  ngOnInit() {
+    console.log("Init");
+  }
+  ngOnDestroy() {
+    console.log("c fuee");
   }
 }
